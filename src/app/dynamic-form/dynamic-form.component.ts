68471d34e473285d2dc66bb05a55ded676063220
longshanp2021 <longshanp2021@gmail.com>
2021-05-07 22:54:26 +0800

    添加表单验证

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -13,19 +13,26 @@ export class DynamicFormComponent implements OnInit {
     private fb: FormBuilder
   ) {
     this.studentInfo = this.fb.group({
-      name: [],
-      age: []
+      name: [undefined, [Validators.required, Validators.minLength(3)]],
+      age: [undefined]
     });
   }
 
+  public get formControls(): { [key: string]: AbstractControl } {
+    return this.studentInfo.controls;
+  }
+
   public ngOnInit(): void {
-    let s1 = { name: 'robot', age: 0 };
-    this.studentInfo.patchValue(s1);
+    // let s1 = { name: 'robot', age: 0 };
+    // this.studentInfo.patchValue(s1);
   }
 
   public save() {
-    let stu = this.studentInfo.value;
-    console.log(stu);
+    // let stu = this.studentInfo.value;
+    // console.log(stu);
+    console.log(1, this.formControls['name']);
+    // console.log(2, this.studentInfo.valid);
+
   }
 
 }

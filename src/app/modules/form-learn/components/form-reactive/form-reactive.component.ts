ec660e84b442dc5bdd3c59b886c0580478feac6d
小蒲 <pulongshan@onerws.com>
2021-07-14 21:28:59 +0800

    1

--- src/app/modules/form-learn/components/form-reactive/form-reactive.component.ts
+++ src/app/modules/form-learn/components/form-reactive/form-reactive.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, AbstractControl, Validators } from "@angular/forms";
+import {debounceTime  } from "rxjs/operators";
+
 @Component({
   selector: 'app-form-reactive',
   templateUrl: './form-reactive.component.html',
   styleUrls: ['./form-reactive.component.scss']
 })
-export class FormReactiveComponent{
+export class FormReactiveComponent implements OnInit {
   personInfo: FormGroup;
+  get formControls(): { [key: string]: AbstractControl } {
+    return this.personInfo.controls;
+  }
   public constructor(
-  public fb: FormBuilder) {
+    public fb: FormBuilder) {
     this.personInfo = this.fb.group({
       name: [undefined, [Validators.required, Validators.minLength(2), Validators.maxLength(4)]],
     });
   }
-  get formControls(): { [key: string]: AbstractControl } {
-    return this.personInfo.controls;
+
+  ngOnInit(): void {
+    this.personInfo.valueChanges
+    .pipe(debounceTime(200))
+    .subscribe(ms => console.log(ms));
   }
+
 
 }
